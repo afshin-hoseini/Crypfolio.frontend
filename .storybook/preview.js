7f87331f5e54c27168f395adaa65b36cb5e70dca
Afshin <afshin.hoseini@gmail.com>
2020-09-19 11:59:40 +0430

    Installed I18next (#13)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,19 @@
 import { StyleProvider } from "../src/Theme";
 import React from "react";
+import { I18nextProvider } from 'react-i18next';
+import '../src/I18n'
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: "^on[A-Z].*" },
 };
 
+
 export const decorators = [
-  (Story) => (
-    <StyleProvider>
-      <Story />
-    </StyleProvider>
-  ),
+    (Story) => (
+        <I18nextProvider>
+            <StyleProvider>
+                <Story />
+            </StyleProvider>
+        </I18nextProvider>
+    ),
 ];
---
+++ src/I18n/resources/en/index.ts
@@ -0,0 +1,5 @@
+import global from './global.json';
+
+export default {
+  global,
+};
